4697428ebdec7408a0d92643cdeacbc313d8e619
Juan David Rodríguez García <juanda@juandarodriguez.es>
2018-03-05 11:15:21 +0100

    correccion

--- ejercicio3/libs/Acme/KeyStorage/KeyDBStorage.php
+++ ejercicio3/libs/Acme/KeyStorage/KeyDBStorage.php
@@ -19,69 +19,38 @@ class KeyDBStorage implements KeyStorageInterface
 
         try
         {
-            $$this->conn = new \PDO($dsn, $username, $password);
+            $this->conn = new \PDO($dsn, $username, $password);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage() . PHP_EOL;
             exit();
         }
-
-        // check key
-
-        $this->valid = true;
-    }
-
-    private function checkKey()
-    {
-        $sql = "SELECT * from clave";
-        $pdo_statement_object = $pdo_object->prepare($sql);
-        $pdo_statement_object->execute();
-        $result = $pdo_statement_object->fetchAll();
-
     }
 
     public function add(KeyRegister $keyregister): bool
     {
-        try {
-            $this->dataJson[$keyregister->name] = [
-                'username' => $keyregister->username,
-                'password' => $keyregister->password,
-                'comment' => $keyregister->comment,
-            ];
-            return true;
-        } catch (\Exception $e) {
-            return false;
-        }
 
     }
 
     public function save(): bool
     {
-        $encodedData = $this->crypter->encrypt(json_encode($this->dataJson));
-        return file_put_contents($this->dataFilePath, $encodedData);
+
     }
 
     public function getAll(): array
     {
-        return $this->dataJson;
+    
     }
 
     public function find(string $name)
     {
 
-        $keyregister = (array_key_exists($name, $this->dataJson)) ?
-        KeyRegister::createFromArray($name, $this->dataJson[$name]) :
-        null;
-
-        return $keyregister;
     }
 
     // unset() no devuelve ningún valor, mantendremos ese criterio en el
     // borrado de elementos.
     public function delete(KeyRegister $keyregister): bool
     {
-        if (!array_key_exists($keyregister->name)) {
-            unset($this->dataJson[$keyregister->name]);
-        }
+
     }
 
     public function __destruct()
